89e5e46585c05d0033322a461a97e6ec9fc664bf
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-02-26 17:16:55 +0500

    add

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 var quetionList = [
 
-{
- 
-    que : "1. HTML stand for?",
-    a : "Hyper Mark Textup Language",
-    b : "Cascading style sheet",
-    c : "php",
-    d : "none"
-    
-}
-,
-{
- 
-    que : "2. CSS stand for?",
-    a : "Hyper Mark Textup Language",
-    b : "Cascading style sheet",
-    c : "php",
-    d : "none"
-    
-},
+    {
 
-{
- 
-    que : "3. HTML stand for?",
-    a : "Hyper Mark Textup Language",
-    b : "Cascading style sheet",
-    c : "php",
-    d : "none"
-    
-},
-{
- 
-    que : "4. CSS stand for?",
-    a : "Hyper Mark Textup Language",
-    b : "Cascading style sheet",
-    c : "php",
-    d : "none"
-    
-},
+        que: "1. HTML stand for?",
+        a: "Hyper Mark Textup Language",
+        b: "Cascading style sheet",
+        c: "php",
+        d: "none"
 
-{
- 
-    que : "5. HTML stand for?",
-    a : "Hyper Mark Textup Language",
-    b : "Cascading style sheet",
-    c : "php",
-    d : "none"
-    
-},
+    }
+    // ,
+    // {
+
+    //     que: "2. CSS stand for?",
+    //     a: "Hyper Mark Textup Language",
+    //     b: "Cascading style sheet",
+    //     c: "php",
+    //     d: "none"
+
+    // },
+
+    // {
+
+    //     que: "3. HTML stand for?",
+    //     a: "Hyper Mark Textup Language",
+    //     b: "Cascading style sheet",
+    //     c: "php",
+    //     d: "none"
+
+    // },
+    // {
+
+    //     que: "4. CSS stand for?",
+    //     a: "Hyper Mark Textup Language",
+    //     b: "Cascading style sheet",
+    //     c: "php",
+    //     d: "none"
+
+    // },
+
+    // {
+
+    //     que: "5. HTML stand for?",
+    //     a: "Hyper Mark Textup Language",
+    //     b: "Cascading style sheet",
+    //     c: "php",
+    //     d: "none"
+
+    // },
 
 
 
@@ -55,20 +55,26 @@ var quetionList = [
 
 
 var questionGenerate = document.getElementById("questiongenerate");
+var a_text = document.getElementById("a_text");
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("c");
+var d = document.getElementById("d");
 var index = 0;
 
 
 
-function load(){
+function load() {
     var data = quetionList[index]
     questionGenerate.innerHTML = `${data.que}`;
     
+
 }
 
 load()
 
 
-function submit(){
+function submit() {
     index++;
     load()
 }
