d87c04865a59be15cf7faeb8f553b67f157941d2
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-02-26 18:34:12 +0500

    add

--- app.js
+++ app.js
@@ -2,50 +2,50 @@ var quetionList = [
 
     {
 
-        que: "1. HTML stand for?",
-        a: "Hyper Mark Textup Language",
-        b: "Cascading style sheet",
-        c: "php",
-        d: "none"
+        que: "1. Which of the following is correct about JavaScript?",
+        a: "The typeof is a unary operator that is placed before its single operand, which can be of any type.",
+        b: "Its value is a string indicating the data type of the operand",
+        c: "Both of the above",
+        d: "None of the above"
 
     }
     ,
     {
 
-        que: "2. CSS stand for?",
-        a: "Hyper Mark Textup Language",
-        b: "php",
-        c: "php",
-        d: "none"
+        que: "2. Which of the following is the correct syntax to print a page using JavaScript?",
+        a: "Using typeof operator",
+        b: "Using getType function",
+        c: "Both of the above",
+        d: "None of the above"
 
     },
 
     {
 
-        que: "3. HTML stand for?",
-        a: "Hyper Mark Textup Language",
-        b: "Cascading style sheet",
-        c: "php",
-        d: "none"
+        que: "3. Which of the following type of variable takes precedence over other if names are same?",
+        a: "Global variable",
+        b: "Local variable",
+        c: "Both of the above",
+        d: "None of the above"
 
     },
     {
 
-        que: "4. CSS stand for?",
-        a: "Hyper Mark Textup Language",
-        b: "Cascading style sheet",
-        c: "php",
-        d: "none"
+        que: "4. Which built-in method returns the characters in a string beginning at the specified location?",
+        a: "substr()",
+        b: "getSubstring()",
+        c: "slice()",
+        d: "None of the above"
 
     },
 
     {
 
-        que: "5. HTML stand for?",
-        a: "Hyper Mark Textup Language",
-        b: "Cascading style sheet",
-        c: "php",
-        d: "none"
+        que: "5. Which of the following function of Number object returns a string value version of the current number?",
+        a: "toString()",
+        b: "toFixed()",
+        c: "toLocaleString()",
+        d: "toPrecision()"
 
     },
 
@@ -67,10 +67,10 @@ var index = 0;
 function load() {
     var data = quetionList[index]
     questionGenerate.innerHTML = `${data.que}`;
-    a_text.innerHTML = `${data.a}`
-    b_text.innerHTML = `${data.b}`
-    c_text.innerHTML = `${data.c}`
-    d_text.innerHTML = `${data.d}`
+    a_text.innerText = `${data.a}`
+    b_text.innerText = `${data.b}`
+    c_text.innerText = `${data.c}`
+    d_text.innerText = `${data.d}`
 
 
 }
